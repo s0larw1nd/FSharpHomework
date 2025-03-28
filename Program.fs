1eb740ebf391b557eada09da5c52a658b1b979f1
s0larw1nd <alixess13@gmail.com>
2025-03-28 18:15:13 +0300

    HW 1 (task 4,5)

--- Program.fs
+++ Program.fs
@@ -7,6 +7,15 @@ let circleVolume radius =
 let cylinderVolume radius length =
     let cir_vol = circleVolume radius
     length * cir_vol
+    
+let rec digSumTop digit =
+    if digit < 10 then digit
+    else digit % 10 + digSumTop (digit / 10)
+        
+let rec digSumTail digit acc=
+    if digit < 10 then acc + digit
+    else
+        digSumTail (digit / 10) (acc + digit % 10)
 
 [<EntryPoint>]
 let main(args : string[]) =
@@ -20,5 +29,12 @@ let main(args : string[]) =
     
     let answ = cylinderVolume radius length
     printfn "%A" answ
+    
+    let dst = digSumTop 123456
+  
+    let digSumTailWrap n = digSumTail n 0
+    let dstl = digSumTailWrap 123456
+    
+    printfn "Вверх: %A\nХвостовая: %A" dst dstl
     
     0
