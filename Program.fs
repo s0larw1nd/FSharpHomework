25f7944af8bd67c812d9d0b1bf413de27a07b11e
s0larw1nd <alixess13@gmail.com>
2025-03-28 17:20:54 +0300

    HW 1 (task 3)

---
+++ Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+let circleVolume radius =
+    let pi = 3.14159
+    pi * radius * radius
+    
+let cylinderVolume radius length =
+    let cir_vol = circleVolume radius
+    length * cir_vol
+
+[<EntryPoint>]
+let main(args : string[]) =
+    printfn "Введите радиус: "
+    let radius_line = Console.ReadLine()
+    let radius = float radius_line
+    
+    printfn "Введите высоту: "
+    let length_line = Console.ReadLine()
+    let length = float length_line
+    
+    let answ = cylinderVolume radius length
+    printfn "%A" answ
+    
+    0
