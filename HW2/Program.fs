01270db14b102f947613932e3c706fd68c89fde6
s0larw1nd <alixess13@gmail.com>
2025-04-04 18:04:54 +0300

    HW 2 (method 2)

--- HW2/Program.fs
+++ HW2/Program.fs
@@ -18,10 +18,21 @@ let rec find_k_even_coprime number temp acc =
     
 let find_k_even_coprime_wrapper number =
     find_k_even_coprime number 2 0
+    
+let rec find_max_digit_not_div_3 num mx =
+    match num with
+    | _ when num=0 -> mx
+    | _ when (num%10) > mx && (num%10)%3<>0 -> find_max_digit_not_div_3 (num/10) (num%10)
+    | _ -> find_max_digit_not_div_3 (num/10) mx
+    
+let find_max_digit_not_div_3_wrapper num =
+    find_max_digit_not_div_3 num 0
 
 [<EntryPoint>]
 let main(args : string[]) =
     let fkecw = find_k_even_coprime_wrapper 104
+    let fmdnd3w = find_max_digit_not_div_3_wrapper 123654
     
-    Console.WriteLine(fkecw);
+    Console.WriteLine(fkecw)
+    Console.WriteLine(fmdnd3w)
     0
