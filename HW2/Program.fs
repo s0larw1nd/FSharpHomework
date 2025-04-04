76e772e0540f335a15930433be6f7594a3cc4bd2
s0larw1nd <alixess13@gmail.com>
2025-04-04 19:13:21 +0300

    HW 2 (method 3)

--- HW2/Program.fs
+++ HW2/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System
 
+// Method 1
 let rec is_coprime digit1 digit2 del =
     match del with
     | _ when del>digit1 -> true
@@ -18,7 +19,8 @@ let rec find_k_even_coprime number temp acc =
     
 let find_k_even_coprime_wrapper number =
     find_k_even_coprime number 2 0
-    
+
+// Method 2
 let rec find_max_digit_not_div_3 num mx =
     match num with
     | _ when num=0 -> mx
@@ -27,12 +29,43 @@ let rec find_max_digit_not_div_3 num mx =
     
 let find_max_digit_not_div_3_wrapper num =
     find_max_digit_not_div_3 num 0
+    
+// Method 3
+let rec find_min_del num del mn =
+    match del with
+    | _ when del = 1 -> mn
+    | _ when num%del=0 -> find_min_del num (del-1) del
+    | _ -> find_min_del num (del-1) mn
+    
+let find_min_del_wrapper num =
+    find_min_del num num num
+    
+let rec find_sum_dig_less_5 num =
+    match num with
+    | _ when num < 5 -> num
+    | _ when num < 10 -> 0
+    | _ when (num%10) < 5 -> (num%10) + find_sum_dig_less_5 (num/10)
+    | _ -> find_sum_dig_less_5 (num/10)
+
+let rec find_max_not_coprime_not_div num temp mx min_del =
+    match temp with
+    | _ when temp = num -> mx
+    | _ when not (is_coprime_wrapper num temp) && temp > mx && temp%min_del<>0 -> find_max_not_coprime_not_div num (temp+1) temp min_del
+    | _ -> find_max_not_coprime_not_div num (temp+1) mx min_del
+    
+let find_max_not_coprime_not_div_wrapper num =
+    find_max_not_coprime_not_div num 2 0 (find_min_del_wrapper num)
+    
+let task_5 num =
+    (find_max_not_coprime_not_div_wrapper num) * (find_sum_dig_less_5 num)
 
 [<EntryPoint>]
 let main(args : string[]) =
     let fkecw = find_k_even_coprime_wrapper 104
     let fmdnd3w = find_max_digit_not_div_3_wrapper 123654
+    let t5 = task_5 130
     
     Console.WriteLine(fkecw)
     Console.WriteLine(fmdnd3w)
+    Console.WriteLine(t5)
     0
