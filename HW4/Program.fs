40692ad73868dda66b92d15652cfdb1a3b6c3b6f
s0larw1nd <alixess13@gmail.com>
2025-04-11 16:57:53 +0300

    HW 4 (task 10)

---
+++ HW4/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+// Task 1
+let rec read_strings_list n =
+    match n with
+    | _ when n=0 -> []
+    | _ -> Console.ReadLine() :: read_strings_list (n-1)
+
+let sort_strings lst =
+    List.sortBy (fun s -> String.length(s)) lst
+
+[<EntryPoint>]
+let main(args : string[]) =
+    //Task 1
+    let str_lst = read_strings_list 5
+    Console.Out.WriteLine(sort_strings str_lst)
+    
+    0
