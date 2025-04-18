231ca19bfcbb639ef7fdf4e4b1d0e9748c89e5d4
s0larw1nd <alixess13@gmail.com>
2025-04-18 19:24:01 +0300

    HW 5 (Lab 7, task 4)

---
+++ HW5/Lab7.fs
@@ -0,0 +1,17 @@
+﻿module Lab7
+open System
+
+let mainLab7() =
+    let consoleAgent = MailboxProcessor.Start(fun inbox ->
+        let rec messageLoop() = async {
+            let! msg = inbox.Receive()
+            printfn "Сообщение: %s" msg
+            if msg = "Time" then printfn "Time: %A" DateTime.Now
+            return! messageLoop()
+        }
+        messageLoop()
+    )
+
+    consoleAgent.Post "Time"
+    System.Threading.Thread.Sleep(100)
+    0
